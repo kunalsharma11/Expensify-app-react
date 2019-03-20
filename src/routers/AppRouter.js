1cbcd7cb8aa22656c2128b4403fb4bfb32845a9a
kunalsharma11 <kunals.eleven@gmail.com>
2019-03-20 01:31:49 -0400

    changes in public routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,9 +6,10 @@ import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
-//import Header from '../components/Header';
+import Header from '../components/Header';
 import LoginPage from '../components/LoginPage';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PrivateRoute';
 
 
 
@@ -20,7 +21,7 @@ const AppRouter = () => (
     <div>
     
     <Switch>
-    <Route path="/" component={LoginPage} exact={true}/>
+    <PublicRoute path="/" component={LoginPage} exact={true}/>
     <PrivateRoute path="/dashboard" component={ExpenseDashboardPage}/>
     <PrivateRoute path="/create" component={AddExpensePage}/>
     <Route path="/help" component={HelpPage}/>
