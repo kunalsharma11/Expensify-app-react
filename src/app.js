292f80c91f8ab4760124c862fa29c47790d31daa
kunalsharma11 <kunals.eleven@gmail.com>
2019-03-20 01:53:13 -0400

    fixed infinite loop bug

--- src/app.js
+++ src/app.js
@@ -1,77 +1,44 @@
-
-//install -> import -> use
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'; //for hoc,(provides store to all the compo. that make up our app)
-import AppRouter, {history} from './routers/AppRouter';
+import { Provider } from 'react-redux';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import {startSetExpense} from './actions/expenses';
-import {login, logout} from './actions/auth';
+import { startSetExpenses } from './actions/expenses';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import {firebase} from './firebase/firebase.js';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
-
-
-
-// setTimeout(() => {
-//   store.dispatch(setTextFilter('bill'));
-// }, 3000)
-
-
-
-//console.log(store.getState());
 const jsx = (
-    <Provider store={store}>
+  <Provider store={store}>
     <AppRouter />
-    </Provider>
+  </Provider>
 );
-
 let hasRendered = false;
 const renderApp = () => {
-    if(!hasRendered){
-        ReactDOM.render(jsx , document.getElementById('app'));
-        hasRendered = true;
-    }
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
 };
 
-ReactDOM.render(<p>Loading...</p> , document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-
-
-firebase.auth().onAuthStateChanged((user) =>{
-    if(user){      
-        store.dispatch(login(user.uid)); 
-        console.log('uid', user.uid);
-        store.dispatch(startSetExpense()).then(() => {
-            renderApp();
-            if(history.location.pathname === '/'){
-                history.push('/dashboard');
-            }
-        });
-    }else{
-        store.dispatch(logout());
-        renderApp();
-        history.push('/');
-    }
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
+  } else {
+    store.dispatch(logout());
+    renderApp();
+    history.push('/');
+  }
 });
-
-// class OldSyntax{
-//     constructor(){
-//         this.name = 'Kunal';
-//     }
-// }
-// const oldSyntax = new OldSyntax();
-// console.log(oldSyntax);
-
-
-// class NewSyntax{
-//     name = 'rohan';
-
-// }
-// const newSyntax = new NewSyntax();
-// console.log(newSyntax);
