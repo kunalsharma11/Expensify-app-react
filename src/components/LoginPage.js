8933023acbf5aa16075813ab55f6087e2edbd10e
kunalsharma11 <kunals.eleven@gmail.com>
2019-03-28 22:54:32 -0400

    beautified login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,9 +3,21 @@ import {connect} from 'react-redux';
 import {startLogin} from '../actions/auth';
 
 export const LoginPage = ({startLogin}) => (
-<div>
-    <button onClick={startLogin}>Login</button>
+<div className=" loginPage">
+<div class="card text-center mx-auto w-35 h-30">
+  <div class="card-header bg-warning">
+    Manage Your Expenses Easily!
+  </div>
+  <div class="card-body bg-dark">
+  <br/>
+    <h2 class="card-title text-white">Login To Manage Your Expenses</h2>
+    <br/><br/>
+    <button className="btn btn-warning btn-lg btn-block bigbutton" onClick={startLogin}>Login</button>
+  </div>
+  </div>
 </div>
+
+
 );
 
 const mapDispatchToProps = (dispatch) => ({
